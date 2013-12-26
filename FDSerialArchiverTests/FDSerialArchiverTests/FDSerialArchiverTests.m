3219ea4e90d8f4742b1759a526d0ffe5f326b2ee
Florian Denis <contact@floriandenis.net>
2013-12-26 21:45:31 +0100

    Created Testing project

---
+++ FDSerialArchiverTests/FDSerialArchiverTests/FDSerialArchiverTests.m
@@ -0,0 +1,30 @@
+//
+//  FDSerialArchiverTests.m
+//  FDSerialArchiverTests
+//
+//  Created by Florian Denis on 26/12/13.
+//  Copyright (c) 2013 Florian Denis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FDSerialArchiverTestsTests : XCTestCase
+
+@end
+
+@implementation FDSerialArchiverTestsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+@end
